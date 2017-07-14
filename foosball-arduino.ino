e6607e98b5975c37e7046070f9b631b0f4e90463
pro585code <pro585g@gmail.com>
2017-07-13 21:29:11 -0400

    changed Wiegand

--- foosball-arduino.ino
+++ foosball-arduino.ino
@@ -5,7 +5,7 @@
 #define ANALOG_INPUTS     4
 
 WIEGAND  wg;
-int ins[6];   // Cannot exceed 6 analog inputs 
+int ins[6];   // Cannot exceed 6 analog inputs
 bool triggered = false;
 static constexpr int THRESHOLD = 22;
 int count = 0;
@@ -14,7 +14,7 @@ int count = 0;
 constexpr char YellowGoalSignal = 'A';
 constexpr char BlackGoalSignal  = 'B';
 constexpr char YellowDropSignal = 'C';
-constexpr char BlackDropSignal  = 'D'; 
+constexpr char BlackDropSignal  = 'D';
 
 constexpr int YellowGoalPin = 0;
 constexpr int BlackGoalPin  = 1;
@@ -39,48 +39,47 @@ void loop()
   for (int j = 0; j < ANALOG_INPUTS; ++j) {
     if (ins[j] < THRESHOLD && !triggered)
     {
-      
+
       triggered = true;
 
-      switch(ins[j]){
+        switch(ins[j]){
 
-        case YellowGoalPin:
-          Serial.print("");
-          Serial.print(YellowGoalSignal);
-          Serial.print("\n");
+          case YellowGoalPin:
+            Serial.print("");
+            Serial.print(YellowGoalSignal);
+            Serial.print("\n");
+            break;
+
+          case BlackGoalPin:
+            Serial.print("");
+            Serial.print(BlackGoalSignal);
+            Serial.print("\n");
+            break;
+
+         case YellowDropPin:
+            Serial.print("");
+            Serial.print(YellowDropSignal);
+            Serial.print("\n");
+            break;
+
+         case BlackDropPin:
+            Serial.print("");
+            Serial.print(BlackDropSignal);
+            Serial.print("\n");
+            break;
+
+        default:
           break;
-
-        case BlackGoalPin:
-          Serial.print("");
-          Serial.print(BlackGoalSignal);
-          Serial.print("\n");
-          break;
-
-       case YellowDropPin:
-          Serial.print("");
-          Serial.print(YellowDropSignal);
-          Serial.print("\n");
-          break;
-
-       case BlackDropPin:
-          Serial.print("");
-          Serial.print(BlackDropSignal);
-          Serial.print("\n");
-          break; 
-
-      default:
-        break;
-        
+        }
       }
     }
-    
+
+    //check for card swipe at end?
     if((wg.available()))
     {
       triggered = false;
       Serial.print("S");
       Serial.print(wg.getCode(),HEX);
       Serial.print("F");
-    }
   }
 }
-
