b8b90d33ff468d1dd15e3ba4937433ca51bc9123
Ryan Ernst <rernst76@gmail.com>
2017-08-28 12:18:07 -0400

    It is broken

--- foosball-arduino.ino
+++ foosball-arduino.ino
@@ -10,73 +10,74 @@ static constexpr int THRESHOLD = 900;
 int count = 0;
 
 
-constexpr char YellowGoalSignal = 'A';
-constexpr char BlackGoalSignal  = 'B';
-constexpr char YellowDropSignal = 'C';
-constexpr char BlackDropSignal  = 'D'; 
+char YellowGoalSignal[] = ´GG´;
+char BlackGoalSignal[]  = ´BG´;
+char YellowDropSignal[] = ´GD´;
+char BlackDropSignal[]  = ´BD´; 
 
 constexpr int YellowGoalPin = 0; // 1010
 constexpr int BlackDropPin  = 1; // 10101
 constexpr int YellowDropPin  = 2; //1010
 constexpr int BlackGoalPin  = 3; //
 
+constexpr int EventDelay = 250;// Time in ms between registering consecutive goals
 void setup()
 {
-  // initialize serial baud. Must be 9600 to conform
-  // with python
-  Serial.begin(230400);
-  wg.begin();
+	// initialize serial baud. Must be 9600 to conform
+	// with python
+	Serial.begin(115200);
+	wg.begin();
 }
 
 void loop()
 {
 
-  for (int i = 0; i < ANALOG_INPUTS; ++i) {
-    ins[i] = analogRead(i);
-  }
+	for (int i = 0; i < ANALOG_INPUTS; ++i) {
+		ins[i] = analogRead(i);
+	}
 
-  for (int j = 0; j < ANALOG_INPUTS; ++j) {
-    if (ins[j] > THRESHOLD)// && !triggered)
-    {
-      
-      switch(j){
+	for (int j = 0; j < ANALOG_INPUTS; ++j) {
+                // Check if a goal is above the goal thresholt
+		if (ins[j] > THRESHOLD) {
 
-        case YellowGoalPin:
-          Serial.print("");
-          Serial.print(YellowGoalSignal);
-          Serial.print("\n");
-          break;
+			// Store timestamp of last goal
+			static unsigned long lastGoal = millis();
 
-        case BlackGoalPin:
-          Serial.print("");
-          Serial.print(BlackGoalSignal);
-          Serial.print("\n");
-          break;
+                        // Ensure that there has been enough time since last event
+			if ( (millis() - lastGoal) >= EventDelay) {
+                                // Print out coresponding event char
+				switch(j){
 
-       case YellowDropPin:
-          Serial.print("");
-          Serial.print(YellowDropSignal);
-          Serial.print("\n");
-          break;
+					case YellowGoalPin:
+						Serial.println(YellowGoalSignal);
+						break;
 
-       case BlackDropPin:
-          Serial.print("");
-          Serial.print(BlackDropSignal);
-          Serial.print("\n");
-          break; 
+					case BlackGoalPin:
+						Serial.println(BlackGoalSignal);
+						break;
 
-      default:
-        break;
-        
-      }
-    }
-  }
-  
-  if(wg.available())
-    {
-      Serial.print("S");
-      Serial.print(wg.getCode(),HEX);
-      Serial.print("F");
-    }
+					case YellowDropPin:
+						Serial.println(YellowDropSignal);
+						break;
+
+					case BlackDropPin:
+						Serial.println(BlackDropSignal);
+						break; 
+
+					default:
+						break;
+				}
+
+                                // Reet lastGoal time
+                                lastGoal = millis();
+			}
+		}
+	}
+
+	if(wg.available())
+	{
+//		Serial.print("S");
+//		Serial.print(wg.getCode(),HEX);
+//		Serial.print("F\n");
+	}
 }
-
