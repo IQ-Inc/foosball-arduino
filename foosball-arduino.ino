bde6853eb738ea413999199d1e067fe1aaf9cbdc
Ryan Ernst <rernst@iq-inc.com>
2018-03-08 18:04:27 -0500

    Ripping out weigand, adding support for Digital.

--- foosball-arduino.ino
+++ foosball-arduino.ino
@@ -1,24 +1,26 @@
 #include "Arduino.h"
 #include "messages.h"
-#include "wiegand/Wiegand.h"
 
 #define ANALOG_INPUTS     4
 
-WIEGAND  wg;
 int ins[6];   // Cannot exceed 6 analog inputs 
 static constexpr int THRESHOLD = 900;
 int count = 0;
-
 
 const char* const YellowGoalSignal = "GG";
 const char* const BlackGoalSignal  = "BG";
 const char* const YellowDropSignal = "GD";
 const char* const BlackDropSignal  = "BD"; 
 
-constexpr int YellowGoalPin = 0; // 1010
-constexpr int BlackDropPin  = 1; // 10101
+constexpr int YellowGoalPin  = 0; // 1010
+constexpr int BlackDropPin   = 1; // 10101
 constexpr int YellowDropPin  = 2; //1010
-constexpr int BlackGoalPin  = 3; //
+constexpr int BlackGoalPin   = 3; //
+
+constexpr int DyellowGoalPin = 7;
+constexpr int DblackGoalPin  = 6;
+constexpr int DyellowDropPin = 5;
+constexpr int DblackDropPin  = 4;
 
 constexpr int EventDelay = 250;// Time in ms between registering consecutive goals
 void setup()
@@ -26,18 +28,52 @@ void setup()
 	// initialize serial baud. Must be 9600 to conform
 	// with python
 	Serial.begin(115200);
-	wg.begin();
+
+  pinMode(DyellowGoalPin, INPUT_PULLUP);
+  pinMode(DblackGoalPin, INPUT_PULLUP);
+  pinMode(DyellowDropPin, INPUT_PULLUP);
+  pinMode(DblackDropPin, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(DyellowGoalPin), yellowGoalHandler, LOW);
+  attachInterrupt(digitalPinToInterrupt(DblackGoalPin), blackGoalHandler, LOW);
+}
+
+void yellowGoalHandler()
+{
+  ins[YellowGoalPin] = THRESHOLD + 1;
+}
+
+void blackGoalHandler()
+{
+  ins[BlackGoalPin] = THRESHOLD + 1;
+}
+
+
+// Since we only have two interrupts poll the ball drops 
+void checkForBallDrops()
+{
+  if(!digitalRead(BlackDropPin))
+  {
+    ins[BlackDropPin] = THRESHOLD + 1;
+  }
+
+  if(!digitalRead(YellowDropPin))
+  {
+    ins[YellowDropPin] = THRESHOLD + 1;
+  }
 }
 
 void loop()
 {
 
+  // Check for analog based events
 	for (int i = 0; i < ANALOG_INPUTS; ++i) {
 		ins[i] = analogRead(i);
+    checkForBallDrops();
 	}
 
 	for (int j = 0; j < ANALOG_INPUTS; ++j) {
-                // Check if a goal is above the goal thresholt
+                // Check if a goal is above the goal threshold
 		if (ins[j] > THRESHOLD) {
 
 			// Store timestamp of last goal
@@ -72,12 +108,5 @@ void loop()
                                 lastGoal = millis();
 			}
 		}
-	}
-
-	if(wg.available())
-	{
-//		Serial.print("S");
-//		Serial.print(wg.getCode(),HEX);
-//		Serial.print("F\n");
 	}
 }
